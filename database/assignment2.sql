9e4f3a18790b2e5660c8b22aff13a64f19ebab1a
Lauren Snyder <lauren@topwebworks.com>
2025-03-29 18:02:23 -0700

    update column name

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -12,7 +12,7 @@ UPDATE inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_model = 'Hummer';
 
-SELECT inventory.make, inventory.model, classification.classification_name
+SELECT inventory.inv_make, inventory.inv_model, classification.classification_name
 FROM inventory
 INNER JOIN classification ON inventory.classification_id = classification.classification_id
 WHERE classification.classification_name = 'Sport';
